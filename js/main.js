a07843746c9dcf418e5d7fca8848e825257fe787
Sergei Piadyshev <s.pyadyshev@gmail.com>
2024-03-21 23:01:55 +0400

    update

--- js/main.js
+++ js/main.js
@@ -1,110 +1,14 @@
 /******/ (function() { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/js/components/mobile-menu.js":
-/*!******************************************!*\
-  !*** ./src/js/components/mobile-menu.js ***!
-  \******************************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   mobileMenu: function() { return /* binding */ mobileMenu; }
-/* harmony export */ });
-var mobileMenu = function () {
-  var init = function init() {
-    var mobileMenuOpen = document.querySelector(".js-open-menu");
-    if (!mobileMenuOpen) {
-      return;
-    }
-    var mobileMenuClose = document.querySelector(".js-close-menu");
-    var body = document.querySelector("body");
-    mobileMenuOpen.addEventListener("click", function () {
-      body.classList.toggle("mobile-menu-active");
-      mobileMenuClose.focus();
-    });
-    mobileMenuClose.addEventListener("click", function () {
-      body.classList.toggle("mobile-menu-active");
-    });
-  };
-  return {
-    init: init
-  };
-}();
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	!function() {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = function(exports, definition) {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	!function() {
-/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-!function() {
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _components_mobile_menu__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/mobile-menu */ "./src/js/components/mobile-menu.js");
+// import { mobileMenu } from "./components/mobile-menu";
 
 window.addEventListener("load", function () {
-  _components_mobile_menu__WEBPACK_IMPORTED_MODULE_0__.mobileMenu.init();
+  // mobileMenu.init();
   document.querySelector("body").classList.add("page-loaded");
 }, false);
-}();
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
